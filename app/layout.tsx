889db77a6b1ea6851f73fe0d9b3c001f55d8b077
gekkinggu <gekkinggu@gmail.com>
2025-06-24 16:58:58 +0700

    dude why

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { AppContextProvider } from "@/context/AppContext";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
